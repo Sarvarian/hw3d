6448d85e88e4b1c2b28b293d5df60b8108861d55
Sarvarian <30783095+Sarvarian@users.noreply.github.com>
2021-04-20 19:38:06 +0430

    put window creation to Window module and create Graphics module

--- main.cpp
+++ main.cpp
@@ -1,58 +1,24 @@
 #include <Windows.h>
+#include <d3d11.h>
 #include <algorithm>
 #include <optional>
 #include "resource.h"
 #include "loguru/loguru.hpp"
 #include "loguru/loguru.cpp"
+#include "Window.cpp"
+#include "Graphics.cpp"
 
 
-HWND build_window(HINSTANCE hInstance, LPCWCHAR pClassName);
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 int Play(const HWND hWnd);
 
 
 int main(int argc, char** argv)
 {
-	constexpr LPCWCHAR pClassName = L"hw3d Oppai";
-	const HMODULE handle = GetModuleHandle(NULL);
 	loguru::init(argc, argv);
-	HWND hWnd = build_window(handle, pClassName);
-	int res = Play(hWnd);
-	UnregisterClassW(pClassName, handle);
+	Window window{ GetModuleHandle(NULL), WndProc };
+	int res = Play(window.hWnd);
 	return res;
-}
-
-HWND build_window(const HINSTANCE hInstance, const LPCWCHAR pClassName)
-{
-	// register window class
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = static_cast<HICON>(LoadImage(hInstance, MAKEINTRESOURCE(IDI_ICON1), IMAGE_ICON, 148, 150, 0));
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
-	RegisterClassEx(&wc);
-
-	// create window instance
-	HWND hWnd = CreateWindowExW(
-		0, pClassName,
-		L"Milk",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		100, 50, 640, 480,
-		nullptr, nullptr, hInstance, nullptr
-	);
-
-	// show the damn window
-	ShowWindow(hWnd, SW_SHOW);
-
-	return hWnd;
 }
 
 LRESULT CALLBACK WndProc(const HWND hWnd, const UINT msg, const WPARAM wParam, const LPARAM lParam)
